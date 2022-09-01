38228d997da36b56490105e368d8493d97ad9882
farhadjaman <89217255+farhadjaman@users.noreply.github.com>
2022-09-01 19:14:20 +0600

    created asyncthunk template file

---
+++ asyncThunkTemplate.js
@@ -0,0 +1,14 @@
+// export function fetchProducts() {
+//     return async function fetchProductThunk(dispatch, getState) {
+//         dispatch(setStatus(STATUSES.LOADING));
+//         try {
+//             const res = await fetch('https://fakestoreapi.com/products');
+//             const data = await res.json();
+//             dispatch(setProducts(data));
+//             dispatch(setStatus(STATUSES.IDLE));
+//         } catch (err) {
+//             console.log(err);
+//             dispatch(setStatus(STATUSES.ERROR));
+//         }
+//     };
+// }
